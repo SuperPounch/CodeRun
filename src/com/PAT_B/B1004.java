bfc060a01212efabf721ce7afe8ba6739467305a
Lixiao <1240847431@qq.com>
2021-09-26 12:25:41 +0800

    fail encode PAT_B:1004 成绩排名（输出最高、最低的姓名和学号；查看Daily_one_problem项目下的代码

---
+++ src/com/PAT_B/B1004.java
@@ -0,0 +1,20 @@
+package com.PAT_B;
+
+import java.util.Scanner;
+
+/* 成绩排名
+输出最高和最低的姓名、学号
+* */
+public class B1004 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        String k = in.nextLine();
+        char[] name= new char[n];
+        char[] number= new char[n];
+        int[] score= new int[n];
+        //需要考虑到读取一行到数据需要处理分成三段数据，不方便
+        //换成C写-->查看Daily-one-problem项目下的代码
+
+    }
+}
